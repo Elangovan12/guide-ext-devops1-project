00aa5a2a7acaed287ed7b9acddb4bfc3859d9296
Elangovan <stellar.cloudworks@outlook.com>
2024-08-02 21:18:44 +0530

    statefile s3 backend & credentials path modified

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = "~> 1.4"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.2"
     }
   }
@@ -11,16 +11,20 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
-  profile = "default"
+  region                   = var.aws_region
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  profile                  = "cloudstudio"
 }
 
 # Create Terraform Backend in S3 and dynamoDB
 terraform {
   backend "s3" {
-    bucket         = "terra-k8swrkspce-state-50987"
-    key            = "remote/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "dynamodb-state-locking"
+    bucket                   = "terra-devops1-state-9998"
+    key                      = "remote/terraform.tfstate"
+    region                   = "ap-south-1"
+    dynamodb_table           = "terraform_state"
+    shared_credentials_files = ["$HOME/.aws/credentials"]
+    profile                  = "cloudstudio"
+
   }
 }
