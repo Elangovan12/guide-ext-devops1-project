70d57b38e583b62cca66c1beeec4a1ba8948c499
Elangovan <stellar.cloudworks@outlook.com>
2024-08-04 21:56:54 +0530

    app vpc addition

---
+++ infra/app-vpc.tf
@@ -0,0 +1,61 @@
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = "ex-${basename(path.cwd)}"
+  region = var.aws_region
+
+  vpc_cidr = "10.200.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.12.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs                 = local.azs
+  private_subnets     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  intra_subnets       = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 20)]
+
+  private_subnet_names = ["Private Subnet One", "Private Subnet Two"]
+  
+  intra_subnet_names       = []
+
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dhcp_options              = true
+  dhcp_options_domain_name         = "stellar.cloudworks"
+  dhcp_options_domain_name_servers = ["10.200.100.1", "10.200.100.2"]
+
+  # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
+  vpc_flow_log_iam_role_name            = "vpc-complete-example-role"
+  vpc_flow_log_iam_role_use_name_prefix = false
+  enable_flow_log                       = true
+  create_flow_log_cloudwatch_log_group  = true
+  create_flow_log_cloudwatch_iam_role   = true
+  flow_log_max_aggregation_interval     = 60
+
+  tags = local.tags
+}
--- infra/auto.tfvars
+++ infra/auto.tfvars
@@ -1,6 +1,6 @@
 # Provider Variables
 aws_region = "ap-south-1"
-workpc_ip  = "134.238.17.163/32"
+workpc_ip  = "137.83.231.181/32"
 # Jenkins Ec2 Variables
 ec2_server_name = "ec2-jenkins"
 ami_id          = "ami-0742fcab3c94e0362"
